2621b579d690b2a6e26c92bda15810b860cf1347
VinceSeely <vince@vinceseely.com>
2015-12-07 10:14:28 -0600

    Stack comments

--- Stack.java
+++ Stack.java
@@ -1,9 +1,9 @@
 /**
-Creates a Stack class, that allows Objects to be added to the Stack,
-or removed and returned. Can check if a Stack is either full, or
-empty, and adds and removes Objects from the top of the Stack.
-
-@author Shane Hendricks
+* Creates a Stack class, that allows Objects to be added to the Stack,
+* or removed and returned. Can check if a Stack is either full, or
+* empty, and adds and removes Objects from the top of the Stack.
+* 
+* @author Vince Seely
 */
 public class Stack 
 
@@ -11,15 +11,15 @@ public class Stack
    private Node stack;
    
    /**
-   Creates a Stack Object, with a size of STACK_SIZE.
+   * Creates a Stack Object, with a size of STACK_SIZE.
    */
    public Stack ()
    {
    }
    
    /**
-   Adds an Object to the top of the Stack, then increments top.
-   @param x is the Object to add to the Stack.
+   * Adds an Object to the top of the Stack, then increments top.
+   * @param x is the Object to add to the Stack.
    */
    public void push ( Object x )
    {
@@ -27,8 +27,8 @@ public class Stack
    }
    
    /**
-   Decrements top, then returns the Object at that position.
-   @return the Object at top
+   * Decrements top, then returns the Object at that position.
+   * @return the Object at top
    */
    public Object pop()
    {
@@ -37,16 +37,27 @@ public class Stack
       return x;
    }
    
+   /**
+    * checks to see if the stack is empty
+    * @return if the first node in the stack is empty
+    */
    public boolean isEmpty()
    {
       return stack == null;
    }
    
+   /**
+    * clears the stack by setting the first element to null;
+    */
    public void clear()
    {
       stack = null;
    }
    
+   /**
+    * test bed main test to make sure the Stack class works.
+    * @param args 
+    */
    public static void main(String [] args)
    {
       Stack s = new Stack();
